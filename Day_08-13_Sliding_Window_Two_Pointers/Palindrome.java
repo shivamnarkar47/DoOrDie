c4afe3ee521e9a687675b2227709738cd7f95e12
Shivam Narkar <shivamnarkar47@gmail.com>
2025-07-01 15:01:24 +0530

    Longest Palindrome Done

---
+++ Day_08-13_Sliding_Window_Two_Pointers/Palindrome.java
@@ -0,0 +1,32 @@
+
+public class Palindrome {
+	public static  int expand(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+
+    public static String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) return "";
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expand(s, i, i);
+            int len2 = expand(s, i, i + 1);
+            int maxLen = Math.max(len1, len2);
+            if (maxLen > end - start) {
+                start = i - (maxLen - 1) / 2;
+                end = i + maxLen / 2;
+            }
+        } 
+        return s.substring(start, end + 1);
+    }
+
+	public static void main(String[] args) {
+		String s = "abbabab";
+		System.out.println("Palindrome : "+longestPalindrome(s));
+		
+	}
+
+}
